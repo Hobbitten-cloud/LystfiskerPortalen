2a911a6b0abdd6bf7e4a3eb4658efe2d9331996e
Hobbitten <janenhansen@gmail.com>
2025-11-07 10:12:37 +0100

    Init branch with some basic properties

--- LystFiskerPortalenWEB/Models/Post.cs
+++ LystFiskerPortalenWEB/Models/Post.cs
@@ -2,6 +2,11 @@
 {
     public class Post
     {
-
+        //public int Id { get; set; }
+        public string Title { get; set; }
+        public string Picture { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public DateTime CreationDate { get; set; }
     }
 }
