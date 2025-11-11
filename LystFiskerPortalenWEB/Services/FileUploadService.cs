98cbdabce328ffc71d8e5e3c9da62994de841896
Søren Frederik Ploug <soren.ploug04@gmail.com>
2025-11-11 13:57:00 +0100

    added username too register form

--- LystFiskerPortalenWEB/Services/FileUploadService.cs
+++ LystFiskerPortalenWEB/Services/FileUploadService.cs
@@ -5,21 +5,20 @@
         async Task<string> UploadFile(IFormFile file)
         {
 
-            //try
-            //{
+            try
+            {
                 var folderPath = Path.Combine("/wwwroot/Images");
                 var filePath = Path.Combine(folderPath, file.FileName);
                 Directory.CreateDirectory(folderPath);
                 await using var stream = new FileStream(filePath, FileMode.Create);
                 await file.CopyToAsync(stream);
-                //string resultMessage = "Upload Succesful";
                 return filePath;
 
-            //}
-            //catch (Exception ex) 
-            //{
-            //    resultMessage = ex.Message;
-            //}
+            }
+            catch (Exception ex) 
+            {
+                return ex.Message;  
+            }
 
         }
     }
