562088ed8c592bfb57977b2fea033bff4b138f57
Daniel Nielsen <danielarnbjerg@hotmail.dk>
2025-11-14 11:00:50 +0100

    Filupload fungerer delvist - man kan i hvert fald åbne en fil

--- LystFiskerPortalenWEB/Services/FileUploadService.cs
+++ LystFiskerPortalenWEB/Services/FileUploadService.cs
@@ -4,22 +4,19 @@
     {
         public async Task<string> UploadFile(IFormFile file)
         {
-
             try
             {
-                var folderPath = Path.Combine("/wwwroot/Images");
+                var folderPath = Path.Combine("Public/TestPictures/");
                 var filePath = Path.Combine(folderPath, file.FileName);
                 Directory.CreateDirectory(folderPath);
                 await using var stream = new FileStream(filePath, FileMode.Create);
                 await file.CopyToAsync(stream);
                 return filePath;
-
             }
             catch (Exception ex)
             {
                 return ex.Message;
             }
-
         }
     }
 }
