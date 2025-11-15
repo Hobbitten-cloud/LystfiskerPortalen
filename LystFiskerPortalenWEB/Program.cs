4bc02cc5e5246693e6f485eaf8debaa177672086
Hobbitten-cloud <janenhansen@gmail.com>
2025-11-15 14:12:23 +0100

    Added interactive server component

--- LystFiskerPortalenWEB/Program.cs
+++ LystFiskerPortalenWEB/Program.cs
@@ -17,7 +17,8 @@ namespace LystFiskerPortalenWEB
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddRazorComponents();
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
 
             builder.Services.AddDbContext<DataContext>(options =>
             {
@@ -74,7 +75,8 @@ namespace LystFiskerPortalenWEB
             app.UseAuthorization();
 
 
-            app.MapRazorComponents<App>();
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
 
             app.MapAdditionalIdentityEndpoints();
 
