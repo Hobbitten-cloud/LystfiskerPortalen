c98402652ae7d7b037f2353efffd75253852172e
Hobbitten <janenhansen@gmail.com>
2025-11-18 13:15:38 +0100

    Editing a post now works - NEEDS MORE WORK THOUGH

--- LystFiskerPortalenWEB/Models/Post.cs
+++ LystFiskerPortalenWEB/Models/Post.cs
@@ -1,16 +1,25 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LystFiskerPortalenWEB.Models
 {
     public class Post
     {
         public int Id { get; set; }
+
         public string? Title { get; set; }
+
         public string? Picture { get; set; }
+
         [Required]
         public string Description { get; set; }
+
         public string? Location { get; set; }
+
         [Required]
         public DateTime CreationDate { get; set; }
+
+        [NotMapped]
+        public bool IsEditing { get; set; }
     }
 }
