bd78cba302f9f2f617d1b4c14c717472da77af4b
Søren Frederik Ploug <soren.ploug04@gmail.com>
2025-11-28 10:30:27 +0100

    push working demo

--- LystFiskerPortalenWEB/Hubs/ChatHub.cs
+++ LystFiskerPortalenWEB/Hubs/ChatHub.cs
@@ -7,9 +7,23 @@ namespace LystFiskerPortalenWEB.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage( string user, string message)
+        public const string HubUrl = "/Chat";
+
+        public async Task Broadcast(string username, string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message); 
+            await Clients.All.SendAsync("Broadcast", username, message);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            Console.WriteLine($"{Context.ConnectionId} connected");
+            return base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception e)
+        {
+            Console.WriteLine($"Disconnected {e?.Message} {Context.ConnectionId}");
+            await base.OnDisconnectedAsync(e);
         }
 
     }
--- LystFiskerPortalenWEB/Program.cs
+++ LystFiskerPortalenWEB/Program.cs
@@ -21,7 +21,7 @@ namespace LystFiskerPortalenWEB
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents();
 
-            builder.Services.AddSignalR();
+            
 
             builder.Services.AddDbContextFactory<DataContext>(options =>
             {
@@ -72,7 +72,7 @@ namespace LystFiskerPortalenWEB
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+      
             app.UseHttpsRedirection();
 
             app.UseAuthentication();
@@ -81,11 +81,11 @@ namespace LystFiskerPortalenWEB
             app.UseStaticFiles();
             app.UseAntiforgery();
 
-            app.MapHub<ChatHub>("/chathub");
+         
 
             app.MapRazorComponents<App>()
                 .AddInteractiveServerRenderMode();
-
+            app.MapHub<ChatHub>("/Chat");
             app.MapAdditionalIdentityEndpoints();
 
             app.Run();
